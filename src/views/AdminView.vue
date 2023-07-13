05a129795d6971fb6e513ab5a1808290019714c1
OlwethuB <olwethu.a.bhiya@gmail.com>
2023-07-13 12:04:39 +0200

    s

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -58,7 +58,7 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-          You are deleting this product, Are you sure?
+          You are deleting this product, Are you sure?...
         ...
         <br>
         <br>
